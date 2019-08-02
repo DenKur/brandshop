34eb599f40085f692b72ed0eccedfdea391206b1
DenKur <kurenkovdenis2012@yandex.ru>
2019-08-02 19:32:44 +0300

    Добавляет файлы сайта

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link href="styles/site.css" rel="stylesheet">
+    <script src="scripts/jquery.js"></script>
+    <script src="scripts/site.js"></script>
+    <title>Онлайн магазин</title>
+</head>
+<body>
+<header>
+    <div id="headerInside">
+        <div id="logo"></div>
+        <div id="companyName">Brand</div>
+        <div id="navWrap">
+            <a href="/">
+                Главная
+            </a>
+            <a href="index.php?page=shop">
+                Магазин
+            </a>
+        </div>
+    </div>
+</header>
+
+<div id="content">
+
+    <?php
+        
+        $goods=[
+            [
+                'id' => 1,
+                'name' => 'Iphone',
+                'desc' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                'img' => '/images/goods/iphone.jpg',
+                'price' => '2000 $'
+            ],
+            [
+                'id' => 2,
+                'name' => 'HTC',
+                'desc' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                'img' => '/images/goods/htc.jpg',
+                'price' => '1200 $'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Samsung',
+                'desc' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                'img' => '/images/goods/samsung.jpg',
+                'price' => '1400 $'
+            ]
+        ];
+
+        $page = $_GET['page'];
+        if(!isset($page))
+            {
+                require('templates/main.php');
+            }
+            else if ($page == 'shop')
+                {
+                    require('templates/shop.php');
+                }
+            else if ($page == 'product')
+                {
+                    $id = $_GET['id'];
+                    $good = [];
+                    foreach ($goods as $product)
+                    {
+                        if($product['id'] == $id)
+                        {
+                            $good = $product;
+                            break;
+                        }
+                    }
+                    require('templates/openProduct.php');
+                }
+
+
+    ?>
+
+</div>
+
+<footer>
+    <div id="footerInside">
+
+        <div id="contacts">
+            <div class="contactWrap">
+                <img src="images/envelope.svg" class="contactIcon">
+                info@brandshop.ru
+            </div>
+            <div class="contactWrap">
+                <img src="images/phone-call.svg" class="contactIcon">
+                8 800 555 00 00
+            </div>
+            <div class="contactWrap">
+                <img src="images/placeholder.svg" class="contactIcon">
+                Москва, пр-т Ленина, д. 1 офис 304
+            </div>
+        </div>
+
+        <div id="footerNav">
+            <a href="/">Главная</a>
+            <a href="index.php?page=shop">Магазин</a>
+        </div>
+
+        <div id="social">
+            <img class="socialItem" src="images/vk-social-logotype.svg">
+            <img class="socialItem" src="images/google-plus-social-logotype.svg">
+            <img class="socialItem" src="images/facebook-logo.svg">
+        </div>
+
+        <div id="copyrights">&copy; Brand</div>
+    </div>
+</footer>
+
+</body>
+</html>
